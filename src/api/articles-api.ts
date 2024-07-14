1bd220fddd73d77f6843be98942c3c51071635e9
DmitriyCherkaska <151064884+DmitriyCherkaska@users.noreply.github.com>
2024-07-15 00:55:54 +0300

    awda

--- src/api/articles-api.ts
+++ src/api/articles-api.ts
@@ -1,23 +1,21 @@
 import axios from 'axios';
-import { Image } from '../types';
 
-const API_KEY = 'nBjDx3M3zD2WrpPhfkSHVh1YXawb-uZYOaY5iVd1jYc';
-axios.defaults.baseURL = 'https://api.unsplash.com';
+const API_KEY = 'HG9aTOmtGBry4YtaddJMiOIW_BQRD2Kv8Luvt536hcs';
+axios.defaults.baseURL = `https://api.unsplash.com`;
 
-const fetchImagesApi = async <T>(
-  searchTerm: string = '',
+const fetchImagesFromApi = async <T>(
+  textInput: string = '',
   page: number,
 ): Promise<T> => {
   const params = new URLSearchParams({
     client_id: API_KEY,
-    query: searchTerm,
+    query: textInput,
     page: page.toString(),
     per_page: '12',
     orientation: 'landscape',
   });
+  const { data } = await axios.get<T>(`/search/photos?${params}`);
 
-  const { data } = await axios.get<T>(`search/photos?${params}`);
   return data;
 };
-
-export default fetchImagesApi;
+export default fetchImagesFromApi;
